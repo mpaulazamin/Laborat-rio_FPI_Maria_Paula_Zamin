ac01096635ee829229c0840bc902168463ed5de0
mpaulazamin <93104147+mpaulazamin@users.noreply.github.com>
2022-04-02 17:46:08 -0300

    Add files via upload

--- Laboratory3/filter_frequency_domain.m
+++ Laboratory3/filter_frequency_domain.m
@@ -1,10 +1,10 @@
-% Aluna: Maria Paula Zamin
-% INF01046 - Laboratório 3
-% Filtragem no domínio frequência
+% Student: Maria Paula Zamin
+% INF01046 - Laboratory number 3
+% Filtering in frequency domain
 
 function filter_frequency_domain(kernel)
 %
-% Q2 (a): read and display the kernel
+% Q2 and Q3 (a): read and display the kernel
 %
 figure;
 subplot(1, 2, 1)
@@ -13,7 +13,7 @@ imshow(filter);
 title('Ideal frequency domain filter');
 
 %
-% Q2 (b): replace 255 values by 1 to have a binary (0, 1) mask
+% Q2 and Q3 (b): replace 255 values by 1 to have a binary (0, 1) mask
 %
 filter_m(:,:) = filter(:,:,1);
 max_value = max(max(filter_m));
@@ -23,7 +23,7 @@ imshow(filter_m, []);
 title('Filter with binary (0, 1) mask');
 
 %
-% Q2 (c): Read cameraman.tif and compute its Fourier transform
+% Q2 and Q3 (c): Read cameraman.tif and compute its Fourier transform
 %
 figure;
 subplot(2, 3, 1)
@@ -33,7 +33,7 @@ title('Original image');
 cman_ft = fft2(cman);
 
 %
-% Q2 (d): Visualize cameraman.tif and its DFT, and apply a shift
+% Q2 and Q3 (d): Visualize cameraman.tif and its DFT, and apply a shift
 %
 subplot(2, 3, 2)
 imshow(log(abs(cman_ft)), [3, 10]);
@@ -45,7 +45,7 @@ imshow(log(abs(cman_ft_shifted)), [3, 10]);
 title('Shifted Spectrum');
 
 %
-% Q2 (e): Multiply the shifted spectrum by the mask
+% Q2 and Q3 (e): Multiply the shifted spectrum by the mask
 %
 cman_ft_shifted_filtered = cman_ft_shifted .* double(filter_m);
 subplot(2, 3, 4)
@@ -53,7 +53,7 @@ imshow(log(abs(cman_ft_shifted_filtered)), [3, 10]);
 title('Filtered Shifted Spectrum');
 
 %
-% Q2 (f): Undo the shift and show the result
+% Q2 and Q3 (f): Undo the shift and show the result
 %
 cman_ft_unshifted_filtered = fftshift(cman_ft_shifted_filtered);
 subplot(2, 3, 5)
@@ -61,7 +61,7 @@ imshow(log(abs(cman_ft_unshifted_filtered)), [3, 10]);
 title('Filtered Spectrum');
 
 %
-% Q2 (g): Apply the inverse Fourier transform and show the filtered image
+% Q2 and Q3 (g): Apply the inverse Fourier transform and show the filtered image
 %
 cman_ift_unshifted_filtered = ifft2(cman_ft_unshifted_filtered);
 subplot(2, 3, 6)
