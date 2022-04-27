1287b4d562e276693c0bc9ce9bf6fc52bdc17134
mpaulazamin <93104147+mpaulazamin@users.noreply.github.com>
2022-04-26 22:53:27 -0300

    Add files via upload

--- Laboratory5/question_five.m
+++ Laboratory5/question_five.m
@@ -116,6 +116,8 @@ title('Negative image in spatial domain');
 % Compute negative of color image
 img_color = imread('Parrots_kodim23.png');
 [rows_color, columns_color, depth_color] = size(img_color );
+Mc = rows_color;
+Nc = columns_color;
 
 redChannel = img_color(:,:,1); 
 greenChannel = img_color(:,:,2); 
@@ -138,7 +140,7 @@ for i = 1 : rows_color
     for j = 1 : columns_color
         pixel_red = img_negative_red(i, j);
           if pixel_red == img_ft_red(1)
-              new_pixel_red = 255 * M * N - img_ft_red(1);
+              new_pixel_red = 255 * Mc * Nc - img_ft_red(1);
           else
               new_pixel_red = img_negative_red(i, j) * -1;
           end
@@ -150,7 +152,7 @@ for i = 1 : rows_color
     for j = 1 : columns_color
         pixel_green = img_negative_green(i, j);
           if pixel_green == img_ft_green(1)
-              new_pixel_green = 255 * M * N - img_ft_green(1);
+              new_pixel_green = 255 * Mc * Nc - img_ft_green(1);
           else
               new_pixel_green = img_negative_green(i, j) * -1;
           end
@@ -162,7 +164,7 @@ for i = 1 : rows_color
     for j = 1 : columns_color
         pixel_blue = img_negative_blue(i, j);
           if pixel_blue == img_ft_blue(1)
-              new_pixel_blue = 255 * M * N - img_ft_blue(1);
+              new_pixel_blue = 255 * Mc * Nc - img_ft_blue(1);
           else
               new_pixel_blue = img_negative_blue(i, j) * -1;
           end
@@ -174,14 +176,14 @@ img_negative_inverse_red = ifft2(img_negative_red);
 img_negative_inverse_blue = ifft2(img_negative_blue);
 img_negative_inverse_green = ifft2(img_negative_green);
 
-img_negative_inverse_color = cat(3, img_negative_inverse_red, img_negative_inverse_blue, img_negative_inverse_green);
+img_negative_inverse_color = cat(3, img_negative_inverse_red, img_negative_inverse_green, img_negative_inverse_blue);
 subplot(1, 3, 2)
-imshow(img_negative_inverse_color);
+imshow(uint8(img_negative_inverse_color));
 title('Negative image recovered');
 
-img_negative_color_spatial = 255 - img_color;
+img_negative_color_spatial(:,:,:) = 255 - img_color(:,:,:);
 subplot(1, 3, 3)
-imshow(img_negative_color_spatial)
+imshow(uint8(img_negative_color_spatial))
 title('Negative image in spatial domain');
 
 end
