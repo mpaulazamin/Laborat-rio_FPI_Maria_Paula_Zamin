884f19990086121f113a17e7c01034441842ae65
mpaulazamin <93104147+mpaulazamin@users.noreply.github.com>
2022-04-26 22:58:53 -0300

    Add files via upload

---
+++ Laboratory5/question_six.m
@@ -0,0 +1,38 @@
+function question_six()
+
+% Original image
+img = imread('cameraman.tif');
+img_ft = fft2(img);
+img_ft_shifted = fftshift(img_ft);
+
+figure;
+subplot(2, 3, 1)
+imshow(img);
+title('Original image');
+
+subplot(2, 3, 2)
+imshow(log(abs(img_ft)), []);
+title('Spectrum');
+
+subplot(2, 3, 3)
+imshow(log(abs(img_ft_shifted)), []);
+title('Shifted spectrum');
+
+% Rotate image
+img_rotated = imrotate(img, 45, 'bilinear', 'crop'); 
+img_ft_rotated = fft2(img_rotated);
+img_ft_rotated_shifted = fftshift(img_ft_rotated);
+
+subplot(2, 3, 4)
+imshow(img_rotated);
+title('Rotated image');
+
+subplot(2, 3, 5)
+imshow(log(abs(img_ft_rotated)), []);
+title('Spectrum rotated');
+
+subplot(2, 3, 6)
+imshow(log(abs(img_ft_rotated_shifted)), []);
+title('Shifted spectrum rotated');
+
+end
